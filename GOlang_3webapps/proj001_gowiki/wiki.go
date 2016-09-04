b4cf745b93493ccd16c2d497f95abd699491221b
atafs <atafs@iscte-iul.pt>
2016-09-04 01:40:53 +0100

    run simple server

--- GOlang_3webapps/proj001_gowiki/wiki.go
+++ GOlang_3webapps/proj001_gowiki/wiki.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
+
 )
 
 //data structure
@@ -11,6 +13,7 @@ type Page struct {
     Body  []byte
 }
 
+//create a simple file page
 func (p *Page) save() error {
     filename := p.Title + ".txt"
     return ioutil.WriteFile(filename, p.Body, 0600)
@@ -25,9 +28,22 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+//create a simple web server
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
+		//create a simple file page
     p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
     p1.save()
     p2, _ := loadPage("TestPage")
     fmt.Println(string(p2.Body))
+
+		//create a simple web server
+		fmt.Println("running the server at localhost:8080...")
+		http.HandleFunc("/", handler)
+	 	http.ListenAndServe(":8080", nil)
+
+
 }
